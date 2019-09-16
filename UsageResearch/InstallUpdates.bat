6b39cc68a0d35550f721237a6cf6592f3a4a8286
ramchandra24 <r.kulkarni@ufl.edu>
2019-09-16 15:27:40 -0400

    Adding signed driver installer

---
+++ UsageResearch/InstallUpdates.bat
@@ -0,0 +1,14 @@
+@echo off
+SET install_dir=%~dp0
+SET faros_domain=http://faros.ece.ufl.edu:12380/
+ 
+cd %install_dir%Driver
+python %install_dir%Driver\unzipDriver.py
+
+cd %install_dir%
+"%install_dir%Driver\devcon.exe" /r install "%install_dir%Driver\KMDFSystemProfiler.inf" Root\FIRMASystemMonitor
+
+@echo off
+ECHO "You are about to restart your machine, please save all your current files/applications"
+PAUSE
+shutdown -r -f -t 10 -c "Reboot System in 10 Seconds"
