6dbad3b10a5f571758db0513cf558b3a7d2797f5
Sanjay PC <sanjayreddy.banda@gmail.com>
2020-09-16 19:51:26 -0400

    1.Removed auto-uninstall 2.Added Force Install python 3.8.5 if user doesnt have python 3.7.3 or greater installed. 3.Capturing DNS Events and Flushing DNS cache for every 5 minutes interval.

--- UsageResearch/Client/sysmon.bat
+++ UsageResearch/Client/sysmon.bat
@@ -3,6 +3,9 @@
 :: Region independent Date
 FOR /F "usebackq tokens=1,2 delims==" %%i IN (`wmic os get LocalDateTime /VALUE 2^>NUL`) DO IF '.%%i.'=='.LocalDateTime.' SET datestr=%%j
 SET datestr=%datestr:~0,4%_%datestr:~4,2%_%datestr:~6,2%
-wevtutil qe "Microsoft-Windows-Sysmon/Operational" /c:1000 /q:"*[System[TimeCreated[timediff(@SystemTime)<300000]]]" /e:Events /f:xml  >> "C:\FIRMA_UserStudy\EventRecord0\%datestr%_SysmonEventLog_%RANDOM%.xml"
-
+SET rand=%RANDOM%
+wevtutil qe "Microsoft-Windows-Sysmon/Operational" /c:1000 /q:"*[System[TimeCreated[timediff(@SystemTime)<300000]]]" /e:Events /f:xml  >> "C:\FIRMA_UserStudy\EventRecord0\%datestr%_SysmonEventLog_%rand%.xml"
+ECHO "Flushing DNS at %datestr% -- %rand%" >> "C:\FIRMA_UserStudy\dnslog.txt"
+ipcongif /flushDNS
+ECHO "DNS Flushing done at %datestr% -- %rand%" >> "C:\FIRMA_UserStudy\dnslog.txt"
 @echo off
